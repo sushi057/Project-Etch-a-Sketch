db0066a411d5922e508a84f5c5ea51465a0bb5a7
sushi057 <champsuvash@gmail.com>
2022-10-06 16:31:45 +0545

    Create Grid

---
+++ script.js
@@ -0,0 +1,16 @@
+const sketchArea = document.querySelector('.sketch');
+
+function createGrid(size){
+    for(let i=0; i<size; i++){
+        for(let j=0; j<size; j++){
+            const gridDiv = document.createElement('div');
+            gridDiv.classList.add('box');
+            gridDiv.style.height = "$(size)px";
+            gridDiv.style.width = "$(size)px";
+
+            sketchArea.appendChild(gridDiv);
+        }
+    }
+}
+
+createGrid(16);
